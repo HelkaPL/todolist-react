7c7f1c5758464a58c144a0bca2aa34d1b9c62e6a
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-01-27 21:34:52 +0100

    added TaskList component

---
+++ src/TasksList/index.js
@@ -0,0 +1,29 @@
+import "./style.css";
+
+const TasksList = ( props ) => (
+    <ul className="section__content">
+        {props.tasks.map(task => (
+            <li
+                key={task.id}
+                className={`taskList${task.done && props.hideDoneTasks ? ' taskList--hidden' : ''}
+                `}>
+            <input
+                type="button" 
+                className="taskList__button taskList__button--add"
+                value={`${task.done ? '✔' : ''}`}
+                />
+            <span 
+                className={`taskList__taskText${task.done ? ' taskList__taskText--done' : ''}`}>
+                {task.content}</span>
+            <input
+                type="button"
+                className="taskList__button taskList__button--remove"
+                value="&#128465;"
+            />
+            </li>
+
+        ))}
+    </ul>
+)
+
+export default TasksList;
