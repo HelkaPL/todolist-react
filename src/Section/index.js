a4a3c392b5421bcbadb547734cede19e802ba602
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-01-27 21:34:35 +0100

    added Section component

---
+++ src/Section/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const Section = ({title, body, extraHeaderContent}) => (
+    <section className="section">
+    <header>
+        <h2 className="section__title">{title}</h2>
+        {extraHeaderContent}
+    </header>
+    <div className="section__content">
+        {body}
+    </div>
+  </section>
+
+);
+
+export default Section;
