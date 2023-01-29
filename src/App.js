6a9754dbe9bdeb9688790d469b6a38117c9a1ee1
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-01-29 18:13:36 +0100

    moved tasks Array into useState const

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Container from "./Container"
 import Header from "./Header";
 import Section from "./Section";
@@ -6,38 +6,39 @@ import Form from "./Form";
 import ExternalsButtons from "./ExternalsButtons";
 import TasksList from "./TasksList";
 
-let tasks = [
-  {
-      id: 1,
-      content: 'Pierwsze zadanie, wciaz niewykonane',
-      done: false,
-  },
-  {
-      id: 2,
-      content: 'Drugie zadanie, już wykonane',
-      done: true,
-  },
-]
 
 function App() {
   const [hideDone, setHideDone] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      content: 'Pierwsze zadanie, wciaz niewykonane',
+      done: false,
+    },
+    {
+      id: 2,
+      content: 'Drugie zadanie, już wykonane',
+      done: true,
+    },
+  ])
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
   }
+
   return (
     <Container>
-        <Header />
-        <Section
-           title="Dodaj nowe zadanie"
-           body={<Form />}
-        />
+      <Header />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
 
-        <Section
-         title="Lista zadań"
-         body={<TasksList tasks={tasks} hideDone={hideDone} />}
-         extraHeaderContent={<ExternalsButtons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone}/>}
-        />
+      <Section
+        title="Lista zadań"
+        body={<TasksList tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={<ExternalsButtons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />}
+      />
     </Container>
   );
 }
